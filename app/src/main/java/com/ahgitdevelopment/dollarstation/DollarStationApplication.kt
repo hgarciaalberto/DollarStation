b06dc412bb811df25d8a00ac3d2a8c647382dab2
hgarciaalberto <hgarcia.alberto@gmail.com>
2022-12-03 00:35:13 +0000

    DS-1 Add hilt dependency

---
+++ app/src/main/java/com/ahgitdevelopment/dollarstation/DollarStationApplication.kt
@@ -0,0 +1,7 @@
+package com.ahgitdevelopment.dollarstation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DollarStationApplication : Application()
