510533ab875d5fa8bd81a2591ee3eff986ee5350
Alberto <51128924+hgarciaalberto@users.noreply.github.com>
2022-12-04 23:16:05 +0000

    DS-4 DS-5 Add swipe refresh. Improve Dashboard Card Item (#6)

--- app/src/main/java/com/ahgitdevelopment/dollarstation/features/dashboard/Dashboard.kt
+++ app/src/main/java/com/ahgitdevelopment/dollarstation/features/dashboard/Dashboard.kt
@@ -1,30 +1,24 @@
 package com.ahgitdevelopment.dollarstation.features.dashboard
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ahgitdevelopment.dollarstation.model.local.Currency
 import com.ahgitdevelopment.dollarstation.ui.theme.DollarStationTheme
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import java.util.Calendar
 
 @OptIn(ExperimentalLifecycleComposeApi::class)
@@ -36,12 +30,19 @@ fun DashboardScreen(
 
     val dollarList by viewModel.dollarList.collectAsStateWithLifecycle()
     val errorMessage by viewModel.errorMessage.collectAsStateWithLifecycle()
+    val isLoading by viewModel.isLoading.collectAsState()
+    val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = isLoading)
 
-    DashboardContent(
-        currencyList = dollarList,
-        error = errorMessage,
-        onClick = viewModel::cardClick
-    )
+    SwipeRefresh(
+        state = swipeRefreshState,
+        onRefresh = viewModel::refreshData
+    ) {
+        DashboardContent(
+            currencyList = dollarList,
+            error = errorMessage,
+            onClick = viewModel::cardClick
+        )
+    }
 }
 
 @Composable
@@ -76,35 +77,6 @@ fun DashboardContent(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun CardItem(
-    item: Currency,
-    modifier: Modifier,
-    onClick: () -> Unit
-) {
-
-    Card(
-        onClick = onClick,
-        modifier = modifier.padding(16.dp),
-        enabled = true,
-        shape = CardDefaults.shape,
-        colors = CardDefaults.cardColors(containerColor = Color.LightGray),
-        elevation = CardDefaults.cardElevation(),
-        border = BorderStroke(2.dp, Color.Blue),
-        interactionSource = remember { MutableInteractionSource() }
-    ) {
-        Column(modifier = modifier.padding(16.dp)) {
-            Text(text = "Currency: ${item.name}")
-            Text(text = "Compra: ${item.buy}")
-            Text(text = "Venta: ${item.sell}")
-            Text(text = "Date: ${item.date}")
-            Text(text = "Variacion: ${item.variation}%")
-        }
-    }
-
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DashboardPreview() {
@@ -115,14 +87,14 @@ fun DashboardPreview() {
             buy = 1.1f,
             sell = 1.1f,
             date = Calendar.getInstance().time,
-            variation = 1.2f,
+            variation = 1.2222f,
         ),
         Currency(
             name = "nacion",
             buy = 1.1f,
             sell = 1.1f,
             date = Calendar.getInstance().time,
-            variation = 1.2f,
+            variation = -1.2f,
         ),
         Currency(
             name = "soja",
--- app/src/main/java/com/ahgitdevelopment/dollarstation/features/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/ahgitdevelopment/dollarstation/features/dashboard/DashboardViewModel.kt
@@ -6,28 +6,37 @@ import com.ahgitdevelopment.dollarstation.model.local.Currency
 import com.ahgitdevelopment.dollarstation.repository.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DashboardViewModel @Inject constructor(
-    repository: Repository
+    private val repository: Repository
 ) : ViewModel() {
 
     val dollarList = MutableStateFlow(listOf<Currency>())
     val errorMessage = MutableStateFlow("")
 
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading = _isLoading.asStateFlow()
+
     init {
-        viewModelScope.launch {
-            repository.getDollars().fold(
-                onSuccess = {
-                    dollarList.value = it
-                },
-                onFailure = {
-                    errorMessage.value = "Error: ${it.message}"
-                }
-            )
-        }
+        refreshData()
+    }
+
+    fun refreshData() = viewModelScope.launch {
+        _isLoading.value = true
+        repository.getDollars().fold(
+            onSuccess = {
+                dollarList.value = it
+                _isLoading.value = false
+            },
+            onFailure = {
+                errorMessage.value = "Error: ${it.message}"
+                _isLoading.value = false
+            }
+        )
     }
 
     fun cardClick() {
